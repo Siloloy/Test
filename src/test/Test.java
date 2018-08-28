27d81130de0b3306ca6b67b7ca68b45de18fb5c5
Hakan Silay <hakansilay1994@hotmail.com>
2018-08-28 17:29:36 +0300

    Boş

--- src/test/Test.java
+++ src/test/Test.java
@@ -28,7 +28,7 @@ public class Test {
         }
         return factors;
     }
-    // dkjasldjaslkjdaskjlddsadsadas
+    // Siloloyyy
     public static boolean primeCheck(int num){
         long startTime = System.nanoTime();
         boolean isPrime=true;
