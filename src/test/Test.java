e939aac1ded8a8fae35f5f8da21371c46e6745dc
hakan <hakan@192.168.1.171>
2018-08-28 16:46:50 +0300

    Boş

--- src/test/Test.java
+++ src/test/Test.java
@@ -28,7 +28,7 @@ public class Test {
         }
         return factors;
     }
-    
+    // djksaldjasljdsakldkjlas
     public static boolean primeCheck(int num){
         long startTime = System.nanoTime();
         boolean isPrime=true;
